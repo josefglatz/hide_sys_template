5cbd2bb8c4a7212ea3db8d95bdfd33c8bb05f5e0
Josef Glatz <typo3@josefglatz.at>
2022-10-18 16:41:53 +0200

    [TASK] Add CGL automation

---
+++ .editorconfig-lint.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return \Symfony\Component\Finder\Finder::create()
+    ->files()
+    ->in(__DIR__)
+    ->ignoreVCSIgnored(true)
+    ->exclude([
+        'Resources/Public/JavaScript',
+        'Resources/Public/Icons',
+    ])
+    ;
